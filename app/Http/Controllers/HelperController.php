102628ed6c6afbb643dba9be548fb7cb3f16b343
larwas <164722305@qq.com>
2018-08-20 09:47:57 +0800

    添加ide-helper扩展，生成权限配置信息

---
+++ app/Http/Controllers/HelperController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Facades\Excel;
+
+class HelperController extends Controller
+{
+    public function geiUserMD5_IMEI()
+    {
+        $use_imei = DB::table('user_imei')->select('imei')->get();
+        $cellData[0] = ['imei'];
+        foreach ($use_imei as $item) {
+            $imei = md5(substr($item->imei, strpos($item->imei, '-')+1)).'\n';
+            array_push($cellData,$imei);
+        }
+
+        Excel::create('imei表',function($excel) use ($cellData){
+            $excel->sheet('order', function($sheet) use ($cellData){
+                $sheet->rows($cellData);
+            });
+        })->export('xlsx');
+    }
+}
