a872b0c6a22d05b9cf565cac7009ff6b9ebb9b82
liuguansheng <164722305@qq.com>
2018-11-21 15:52:10 +0800

    修复手机端页面无法缩放问题

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title','综合后台')</title>
     <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    {{--<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes" name="viewport">--}}
     <!-- Bootstrap 3.3.7 -->
     <link rel="stylesheet" href="/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
     <!-- Font Awesome -->
