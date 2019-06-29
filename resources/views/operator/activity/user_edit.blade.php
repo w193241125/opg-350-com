30834cc6d8137570b242351d06c7895a9792e2d2
test <test@test.com>
2019-06-29 15:56:57 +0800

    活动添加用户列表

---
+++ resources/views/operator/activity/user_edit.blade.php
@@ -0,0 +1,50 @@
+<div class="modal-header">
+    <h4 class="modal-title" id="myModalLabel">
+        编辑用户充值信息
+    </h4>
+</div>
+<div class="modal-body">
+    <div class="form-group">
+        <label for="server">区服</label>
+        <input type="text" class="form-control" id="server" value="{{$data->server_name}}" disabled="disabled">
+    </div>
+    <div class="form-group">
+        <label for="role_name">角色名</label>
+        <input type="text" class="form-control" id="role_name" value="{{$data->role_name}}" disabled="disabled">
+    </div>
+    <div class="form-group">
+        <label for="total">充值金额</label>
+        <input type="text" class="form-control" id="upd_total" name="total" value="{{$data->total}}">
+    </div>
+    <input type="hidden" class="form-control" id="upd_id" name="total" value="{{$data->id}}">
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">暂不修改
+    </button>
+    <button type="button" class="btn btn-primary" id="bind_btn" onclick="upd_user();">
+        更新
+    </button>
+</div>
+
+<script>
+    function upd_user(){
+        var id = $('#upd_id').val()
+        var total = $('#upd_total').val()
+        $.ajax({
+            'type' : 'POST',
+            'url' :  '/operator/user_upd',
+            data: {id: id,total:total},
+            headers : {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success : function (res) {
+                if(res.status==200){
+                    swal("更新成功！", res.message, "success");
+                    location.reload()
+                }else{
+                    swal("更新！", res.message, "error");
+                }
+            }
+        });
+    }
+</script>
---
+++ resources/views/operator/activity/xlczg.blade.php
@@ -0,0 +1,181 @@
+@extends('layouts.app')
+
+@section('title', '用户管理')
+{{--顶部前端资源--}}
+@section('styles')
+    <style>
+        .imp{color: red}
+        .font_style{color: #999;font-size: 18px;}
+    </style>
+    <!-- 引入添加菜单的样式 -->
+    <link href="{{asset('assets/admin/layouts/css/components-md.min.css')}}" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="{{asset('assets/admin/layouts/css/plugins-md.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- 引入添加菜单的样式结束 -->
+@endsection
+
+{{--页面内容--}}
+@section('contents')
+    <div class="content-wrapper" style="min-height: 960px;">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                用户配置
+                <small>Version 1.0</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> 系统设置</a></li>
+                <li class="active">用户配置</li>
+            </ol>
+        </section>
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <script>
+                    //实时判断用户名是否存在
+                    $("#form_mark").bind("input propertychange",function(event){
+                        mark = $("#form_mark").val();
+                        url =  "{{ route('ajaxGetMarkIfExist') }}";
+                        $.getJSON(
+                            url,
+                            {mark:mark},
+                            function (data) {
+                                if (data.length != 0) {
+                                    console.log(data)
+                                    $("#form_mark").parent('div').addClass('has-error');
+                                    $(".form_mark").html('标识已存在');
+                                    $(".form_mark").css('color','red');
+                                }else{
+                                    $("#form_mark").parent('div').removeClass('has-error');
+                                    $(".form_mark").html('使用：五等奖，以此类推... 标识√');
+                                    $(".form_mark").css('color','#36c6d3');
+                                }
+                            }
+                        );
+                    });
+
+                </script>
+                {{--其它--}}
+                <div class="col-xs-4">
+                    <div class="box">
+                        <!-- /.box-header -->
+                        <div class="portlet light bordered formBox" id="lottery_three">
+                            <div class="portlet-title">
+                                <div class="caption font-green">
+                                    <i class="icon-pin font-green"></i>
+                                    <span class="caption-subject bold uppercase">添加用户</span>
+                                </div>
+                                <div class="actions">
+                                    <a class="btn btn-circle btn-icon-only btn-default close-link">
+                                        <i class="fa fa-times"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="portlet-body form">
+                                @if (count($errors) > 0)
+                                    <div class="alert alert-danger">
+                                        <strong>信息填写出错!</strong>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </div>
+                                @endif
+                                <form role="form" id="lottery_three_Form">
+                                    {{ csrf_field() }}
+                                    <div class="form-body">
+                                        <div class="form-group form-md-line-input form-md-floating-label
+                <?php if ($errors->has('server_name')) { echo "has-error"; } ?> ">
+                                            <input type="text" class="form-control" id="form_server_name" name="server_name" value="{{ old('server_name') }}">
+                                            <label for="form_server_name"><span class="imp">*&nbsp;</span>区服ID</label>
+                                            <span class="help-block form_server_name">如：1服</span>
+                                        </div>
+                                        <div class="form-group form-md-line-input form-md-floating-label
+                <?php if ($errors->has('role_name')) { echo "has-error"; } ?>">
+                                            <input type="text" class="form-control" id="form_role_name" name="role_name" value="{{ old('role_name') }}">
+                                            <label for="form_role_name"><span class="imp">*&nbsp;</span>角色名</label>
+                                            <span class="help-block">请填写角色名</span>
+                                        </div>
+                                        <div class="form-group form-md-line-input form-md-floating-label
+                <?php if ($errors->has('total')) { echo "has-error"; } ?>">
+                                            <input type="text" class="form-control" id="form_total" name="total" value="{{ old('total') }}">
+                                            <label for="form_total"><span class="imp">*&nbsp;</span>充值金额</label>
+                                            <span class="help-block">请填写充值金额</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-actions noborder">
+                                        <button type="submit" class="btn green lottery_three" >添加用户</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <script>
+                    $('.lottery_three').on('click',function () {
+                        console.log('lottery_three')
+                        var _item = $(this);
+                        var _form = $('#lottery_three_Form');
+                        console.log(_form.serializeArray());
+                        $.ajax({
+                            url: '/operator/add_user',
+                            type:'post',
+                            dataType: 'json',
+                            data:_form.serializeArray(),
+                            headers : {
+                                'X-CSRF-TOKEN': $("input[name='_token']").val()
+                            },
+                            beforeSend : function(){
+                                _item.attr('disabled','true');
+                            },
+                            success:function (response) {
+                                sweetAlert(response.message);
+                            }
+                        }).fail(function(response) {
+                            if(response.status == 422){
+                                var data = $.parseJSON(response.responseText);
+                                var layerStr = "";
+                                for(var i in data.errors){
+                                    layerStr += data.errors[i]+" ";
+                                }
+                                sweetAlert('错误', layerStr);
+                            }
+                        }).always(function () {
+                            _item.removeAttr('disabled');
+                        });
+                    });
+                    //实时判断用户名是否存在
+                    $("#form_username").bind("input propertychange",function(event){
+                        uid = $("#form_username").val();
+                        url =  "{{ route('ajaxcheckUid') }}";
+                        $.getJSON(
+                            url,
+                            {uid:uid},
+                            function (data) {
+                                if (data.length != 0) {
+                                    console.log(data)
+                                    $("#form_username").parent('div').addClass('has-error');
+                                    $(".form_username").html('用户ID已存在');
+                                    $(".form_username").css('color','red');
+                                }else{
+                                    $("#form_username").parent('div').removeClass('has-error');
+                                    $(".form_username").html('用户名');
+                                    $(".form_username").css('color','#36c6d3');
+                                }
+                            }
+                        );
+                    });
+                </script>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
+
+
+{{--尾部前端资源--}}
+@section('script')
+    <!-- BEGIN THEME GLOBAL SCRIPTS 这个js控制 添加菜单 的 label 上移与下移 -->
+    <script src="{{asset('assets/admin/layouts/scripts/app.min.js')}}" type="text/javascript"></script>
+@endsection
